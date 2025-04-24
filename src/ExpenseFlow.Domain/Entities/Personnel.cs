7f4c7ca0d31ca6e5b27a453d7516cc045ca62fac
Nigarselekk <nigarselek923@gmail.com>
2025-04-24 14:15:40 +0300

    Add Personnel entity

---
+++ src/ExpenseFlow.Domain/Entities/Personnel.cs
@@ -0,0 +1,13 @@
+namespace ExpenseFlow.Domain.Entities;
+
+    public class Personnel
+    {
+        public Guid Id { get; set; }
+        public string FirstName { get; set; } = null!;
+        public string LastName { get; set; } = null!;
+        public string Email { get; set; } = null!;
+        public string PhoneNumber { get; set; } = null!;
+        public string Address { get; set; } = null!;
+        public string City { get; set; } = null!;
+    
+    }
