860d2d0fe7270e78e23ca39b9d10ad961aea49ae
Nigarselekk <nigarselek923@gmail.com>
2025-04-24 15:09:17 +0300

    Add PaymentTransaction entity

---
+++ src/ExpenseFlow.Domain/Entities/PaymentTransaction.cs
@@ -0,0 +1,16 @@
+namespace ExpenseFlow.Domain.Entities;
+
+    public enum TransferType { EFT, Havale }
+    public enum TransactionStatus { Success, Failed }
+
+    public class PaymentTransaction
+    {
+        public Guid Id { get; set; }
+        public Guid ExpenseId { get; set; }
+
+        public DateTime TransactionDate { get; set; } = DateTime.UtcNow;
+        public decimal Amount { get; set; }
+        public TransferType TransferType { get; set; }  = TransferType.EFT;
+        public TransactionStatus Status { get; set; }
+    }
+
